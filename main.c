3f0f449179817925b37de2f605e379766f452c68
michael-christen <mchristen96@gmail.com>
2018-09-11 21:36:10 -0700

    Get it spinning easy

--- main.c
+++ main.c
@@ -4,40 +4,68 @@
 #include "stm32f0xx.h"
 #include "stm32f0xx_conf.h"
 
-#define BLINK_DELAY_MS	(500)
+#define BLINK_DELAY_US	(20)
 
-volatile uint32_t tickMs = 0;
+volatile uint32_t tickUs = 0;
 
 void init() {
 	// ---------- SysTick timer -------- //
-	if (SysTick_Config(SystemCoreClock / 1000)) {
+	if (SysTick_Config(SystemCoreClock / 1000000)) {
 		// Capture error
 		while (1){};
 	}
-
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-	GPIO_Init(GPIOC, &(GPIO_InitTypeDef){GPIO_Pin_9, GPIO_Mode_OUT, GPIO_Speed_2MHz, GPIO_OType_PP, GPIO_PuPd_NOPULL});
 }
 
+typedef struct stepper_t {
+    uint32_t next_step;
+    uint8_t state;
+    uint32_t step_delay;
+    GPIO_TypeDef *gpio_port;
+    uint32_t gpio_pin;
+} stepper_t;
+
+
+stepper_t stepper_init(GPIO_TypeDef *gpio_port, uint32_t gpio_pin, uint32_t step_delay) {
+    stepper_t this_stepper = {
+        .next_step = tickUs + step_delay,
+        .state = 0,
+        .step_delay = step_delay,
+        .gpio_port = gpio_port,
+        .gpio_pin = gpio_pin,
+    };
+    return this_stepper;
+}
+
+void next_action(stepper_t *stepper) {
+	if(tickUs > stepper->next_step) {
+        stepper->next_step = stepper->step_delay + tickUs;
+		if(stepper->state) {
+			GPIO_SetBits(stepper->gpio_port, stepper->gpio_pin);
+		} else {
+			GPIO_ResetBits(stepper->gpio_port, stepper->gpio_pin);
+		}
+		stepper->state ^= 1;
+	}
+}
+
+
 int main(void) {
-	uint32_t nextBlink;
-	uint32_t blinkState = 0;
 
 	init();
 
-	nextBlink = tickMs + BLINK_DELAY_MS;
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
+	GPIO_Init(
+        GPIOC,
+        &(GPIO_InitTypeDef){
+            GPIO_Pin_9,
+            GPIO_Mode_OUT,
+            GPIO_Speed_2MHz,
+            GPIO_OType_PP,
+            GPIO_PuPd_NOPULL
+        });
+    stepper_t stepper0 = stepper_init(GPIOC, GPIO_Pin_9, BLINK_DELAY_US);
 	for(;;) {
-
-		if(tickMs > nextBlink) {
-			nextBlink = tickMs + BLINK_DELAY_MS;
-			if(blinkState) {
-				GPIO_SetBits(GPIOC, GPIO_Pin_9);
-			} else {
-				GPIO_ResetBits(GPIOC, GPIO_Pin_9);
-			}
-			blinkState ^= 1;
-		}
-
+        next_action(&stepper0);
 		__WFI();
 	}
 
@@ -46,5 +74,5 @@ int main(void) {
 
 void SysTick_Handler(void)
 {
-	tickMs++;
+	tickUs++;
 }
