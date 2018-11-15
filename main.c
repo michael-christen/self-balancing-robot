cefddac4a57b6010945a5842be8e2b982cc1b4cb
michael-christen <mchristen96@gmail.com>
2018-11-14 23:27:01 -0800

    Get it kinda working

--- main.c
+++ main.c
@@ -152,8 +152,8 @@ int delay_profile_example(void) {
 }
 
 
-// TODO: Update after removing quaternion math...
-#define PERIOD 4000  // uS
+// TODO: Improve
+#define PERIOD 13000  // uS ~ 75 Hz
 
 
 int16_t constr(int16_t value, int16_t smallest, int16_t biggest) {
@@ -266,14 +266,12 @@ int main(void) {
     uint32_t last_display = tickUs;
     uint32_t last_loop;
 
-
-    float roll = 0, pitch = 0, rollAcc = 0, pitchAcc = 0;
     float speed = 0;
 
     float integral_error = 0;
     float last_pid_error = 0;
     float pid_output = 0;
-    const float Kp = 4000.0;
+    const float Kp = 100.0;
     const float Ki = 0.0;
     const float Kd = 0.0;
     const float MAX_SPEED = 45000;
@@ -284,44 +282,9 @@ int main(void) {
         last_loop = tickUs;
         imu_update_quaternion();
         imu_get_euler_orientation(&angles);
+        // TODO: Proper PID
 
-        // Accelerometer data
-        usart_send_string("ACC: ");
-        ftoa(c_str, orientation.ax, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, orientation.ay, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, orientation.az, 2); usart_send_string(c_str);
-        usart_send_string("\r\n");
-
-        // Gyroscopic Data
-        usart_send_string("GYR: ");
-        ftoa(c_str, orientation.gx, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, orientation.gy, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, orientation.gz, 2); usart_send_string(c_str);
-        usart_send_string("\r\n");
-
-        // Magnetometer data
-        usart_send_string("MAG: ");
-        ftoa(c_str, orientation.mx, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, orientation.my, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, orientation.mz, 2); usart_send_string(c_str);
-        usart_send_string("\r\n");
-
-        // Yaw, Pitch, Roll
-        usart_send_string("Orientation: ");
-        ftoa(c_str, angles.yaw, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, angles.pitch, 2); usart_send_string(c_str);
-        usart_send_string(" ");
-        ftoa(c_str, angles.roll, 2); usart_send_string(c_str);
-        usart_send_string("\r\n");
-
-        float value = orientation.ay;
+        float value = angles.roll;
 
         float pid_error = value;
         integral_error += Ki * pid_error;
@@ -329,16 +292,21 @@ int main(void) {
         float error_derivative = pid_error - last_pid_error;
         pid_output = Kp * pid_error + integral_error + Kd * error_derivative;
 
-        if (value > 0.75 || value < -0.75) {
+        if (value > 60 || value < -60) {
             pid_output = 0;
             integral_error = 0;
         }
         last_pid_error = pid_error;
+
         float speed = (
             constrf(pid_output, -MAX_PID_OUTPUT, MAX_PID_OUTPUT) *
             (MAX_SPEED / MAX_PID_OUTPUT));
-        motor_speed = (uint16_t)abs(speed);
         bool dir = (speed < 0);
+        if (speed < 0) {
+            motor_speed = (uint16_t) (-speed);
+        } else {
+            motor_speed = (uint16_t) speed;
+        }
         stepper_set_dir(&stepper0, dir);
         stepper_set_speed(&stepper0, motor_speed);
         /*
@@ -354,39 +322,37 @@ int main(void) {
             usart_send_string("PID: ");
             ftoa(c_str, pid_output, 2); usart_send_string(c_str);
             usart_send_string("\r\n");
-
-            usart_send_string("SPD: ");
-            ftoa(c_str, speed, 2); usart_send_string(c_str);
-            usart_send_string(" ");
-            itoa(c_str, motor_speed, 2); usart_send_string(c_str);
-            usart_send_string("\r\n");
-            */
-
-            /*
-            usart_send_string("NEW: ");
-            ftoa(c_str, roll, 2); usart_send_string(c_str);
-            usart_send_string(" ");
-            ftoa(c_str, pitch, 2); usart_send_string(c_str);
-            usart_send_string("\r\n");
             */
 
 
-            /*
-            // Quaternion
-            float *q = getQ();
-            usart_send_string("QTN: ");
-            ftoa(c_str, q[0], 2); usart_send_string(c_str);
+            // Accelerometer data
+            usart_send_string("ACC: ");
+            ftoa(c_str, orientation.ax, 2); usart_send_string(c_str);
             usart_send_string(" ");
-            ftoa(c_str, q[1], 2); usart_send_string(c_str);
+            ftoa(c_str, orientation.ay, 2); usart_send_string(c_str);
             usart_send_string(" ");
-            ftoa(c_str, q[2], 2); usart_send_string(c_str);
-            usart_send_string(" ");
-            ftoa(c_str, q[3], 2); usart_send_string(c_str);
+            ftoa(c_str, orientation.az, 2); usart_send_string(c_str);
             usart_send_string("\r\n");
-            */
-
+    
+            // Gyroscopic Data
+            usart_send_string("GYR: ");
+            ftoa(c_str, orientation.gx, 2); usart_send_string(c_str);
+            usart_send_string(" ");
+            ftoa(c_str, orientation.gy, 2); usart_send_string(c_str);
+            usart_send_string(" ");
+            ftoa(c_str, orientation.gz, 2); usart_send_string(c_str);
+            usart_send_string("\r\n");
+    
+            // Magnetometer data
+            usart_send_string("MAG: ");
+            ftoa(c_str, orientation.mx, 2); usart_send_string(c_str);
+            usart_send_string(" ");
+            ftoa(c_str, orientation.my, 2); usart_send_string(c_str);
+            usart_send_string(" ");
+            ftoa(c_str, orientation.mz, 2); usart_send_string(c_str);
+            usart_send_string("\r\n");
+    
             // Yaw, Pitch, Roll
-            /*
             usart_send_string("Orientation: ");
             ftoa(c_str, angles.yaw, 2); usart_send_string(c_str);
             usart_send_string(" ");
@@ -394,15 +360,18 @@ int main(void) {
             usart_send_string(" ");
             ftoa(c_str, angles.roll, 2); usart_send_string(c_str);
             usart_send_string("\r\n");
-            */
+
+            usart_send_string("Speed: ");
+            ftoa(c_str, speed, 2); usart_send_string(c_str);
+            usart_send_string(" ");
+            itoa(c_str, motor_speed, 2); usart_send_string(c_str);
+            usart_send_string("\r\n");
 
             last_display = tickUs;
         }
-        /*
         if ((tickUs - last_loop) > PERIOD) {
             usart_send_string("ERROR loop too short\r\n");
         }
-        */
         while((tickUs - last_loop) < PERIOD);
     }
     for(;;);
